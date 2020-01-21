1acc68e5f41ebc77df8d889d5114897df7ae8fcb
macsitta <59915964+macsitta@users.noreply.github.com>
2020-01-21 23:28:13 +0700

    lab 5
    - สร้าง Activity ในโปรเจค StyleDemoApp เลยได้แอพชื่อเดิมครับ

---
+++ lab5/StyleDemoApp/app/src/main/java/com/example/styledemoapp/Movie_List.kt
@@ -0,0 +1,12 @@
+package com.example.styledemoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Movie_List : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie__list)
+    }
+}
---
+++ lab5/StyleDemoApp/app/src/main/java/com/example/styledemoapp/Writer_list.kt
@@ -0,0 +1,12 @@
+package com.example.styledemoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Writer_list : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_writer_list)
+    }
+}
