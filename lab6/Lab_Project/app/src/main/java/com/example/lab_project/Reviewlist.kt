ca7d39b8f4d7f3bf9b0539e7a2423dc6ff95496b
macsitta <59915964+macsitta@users.noreply.github.com>
2020-02-04 15:17:55 +0700

    lab6

---
+++ lab6/Lab_Project/app/src/main/java/com/example/lab_project/Reviewlist.kt
@@ -0,0 +1,12 @@
+package com.example.lab_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Reviewlist : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reviewlist)
+    }
+}
